366fe77374f1b67952e94e9486f9fb30e0af8601
takashi3802 <shikataudone@yahoo.co.jp>
2021-03-10 16:03:34 +0900

    ユーザー編集削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      sign_in(current_user, bypass: true)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :age, :gender, :occupation, :phone, :trouble)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "staffs#index"
   resources :staffs, only: [:new, :create, :index]
+  resources :users, only: [:show, :update, :edit]
 end
